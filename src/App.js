ffb7372d85700074694811f3cf06b3f56a042003
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-20 21:58:34 -0400

    adds image

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
     <div className="App">
       <h1>Daily Outlook?</h1>
       <div className="weather">{weather}</div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7QXZwhSE2VriMcdx_06_43Q9Nf3fvAX13uw&usqp=CAU"/>
     </div>
   );
 }
