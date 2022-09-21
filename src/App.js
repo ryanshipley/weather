32b28aec0a54d19d90e5504b69a9b16a039fb502
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-20 21:41:23 -0400

    maps weather array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import './App.css';
 import WeatherForecast from './components/WeatherForcast';
 import Weather from './WeatherData';
 
+const weather = Weather.map((ele, index) =>{
+  return(
+    <div>
+      <WeatherForecast
+      img={ele.img}
+      condition={ele.condition}
+      time={ele.time}
+      key={index}
+      />
+    </div>
+  );
+});
+
 function App() {
   return (
     <div className="App">
